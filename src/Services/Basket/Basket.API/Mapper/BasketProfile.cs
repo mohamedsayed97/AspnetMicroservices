1033143731c03b25bd61f14159d0d038498392ed
m.sayed <m.sayed@egyptianbanks.net>
2023-06-14 23:18:50 +0300

    push basket service

--- src/Services/Basket/Basket.API/Mapper/BasketProfile.cs
+++ src/Services/Basket/Basket.API/Mapper/BasketProfile.cs
@@ -1,14 +1,14 @@
-﻿using AutoMapper;
-using Basket.API.Entities;
-using EventBus.Messages.Events;
+﻿//using AutoMapper;
+//using Basket.API.Entities;
+//using EventBus.Messages.Events;
 
-namespace Basket.API.Mapper
-{
-    public class BasketProfile : Profile
-	{
-		public BasketProfile()
-		{
-			CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
-		}
-	}
-}
+//namespace Basket.API.Mapper
+//{
+//    public class BasketProfile : Profile
+//	{
+//		public BasketProfile()
+//		{
+//			CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+//		}
+//	}
+//}
