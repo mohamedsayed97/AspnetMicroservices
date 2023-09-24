14700697390119a866d9b515bd33ff12aa11727c
m.sayed <m.sayed@egyptianbanks.net>
2023-09-24 14:49:28 +0300

    From Section 8

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,9 +1,9 @@
-﻿//using AutoMapper;
+﻿using AutoMapper;
 using Basket.API.Entities;
-//using Basket.API.GrpcServices;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories.Interfaces;
-//using EventBus.Messages.Events;
-//using MassTransit;
+using EventBus.Messages.Events;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net;
@@ -16,18 +16,20 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
-        //private readonly DiscountGrpcService _discountGrpcService;
+        private readonly DiscountGrpcService _discountGrpcService;
         //private readonly IPublishEndpoint _publishEndpoint;
-        //private readonly IMapper _mapper;
+        private readonly IMapper _mapper;
 
         public BasketController(IBasketRepository repository
-            //, DiscountGrpcService discountGrpcService,IPublishEndpoint publishEndpoint, IMapper mapper
+            , DiscountGrpcService discountGrpcService
+            //,IPublishEndpoint publishEndpoint
+            , IMapper mapper
             )
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
-            //_discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
             //_publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
-            //_mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
         
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -45,8 +47,8 @@ namespace Basket.API.Controllers
             // Communicate with Discount.Grpc and calculate lastest prices of products into sc
             foreach (var item in basket.Items)
             {
-                //var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
-                item.Price -= 0;
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
             }
 
             return Ok(await _repository.UpdateBasket(basket));
@@ -60,33 +62,33 @@ namespace Basket.API.Controllers
             return Ok();
         }
 
-        //[Route("[action]")]
-        //[HttpPost]
-        //[ProducesResponseType((int)HttpStatusCode.Accepted)]
-        //[ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        //public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
-        //{
-        //    // get existing basket with total price            
-        //    // Set TotalPrice on basketCheckout eventMessage
-        //    // send checkout event to rabbitmq
-        //    // remove the basket
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            // get existing basket with total price            
+            // Set TotalPrice on basketCheckout eventMessage
+            // send checkout event to rabbitmq
+            // remove the basket
 
-        //    // get existing basket with total price
-        //    var basket = await _repository.GetBasket(basketCheckout.UserName);
-        //    if (basket == null)
-        //    {
-        //        return BadRequest();
-        //    }
+            // get existing basket with total price
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+            if (basket == null)
+            {
+                return BadRequest();
+            }
 
-        //    // send checkout event to rabbitmq
-        //    var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
-        //    eventMessage.TotalPrice = basket.TotalPrice;
-        //    await _publishEndpoint.Publish<BasketCheckoutEvent>(eventMessage);
+            // send checkout event to rabbitmq
+            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMessage.TotalPrice = basket.TotalPrice;
+            //await _publishEndpoint.Publish<BasketCheckoutEvent>(eventMessage);
 
-        //    // remove the basket
-        //    await _repository.DeleteBasket(basket.UserName);
+            // remove the basket
+            await _repository.DeleteBasket(basket.UserName);
 
-        //    return Accepted();
-        //}
+            return Accepted();
+        }
     }
 }
--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,25 +1,19 @@
-using Basket.API.GrpcServices;
-using Basket.API.Repositories;
-using Basket.API.Repositories.Interfaces;
-using Discount.Grpc.Protos;
-//using HealthChecks.UI.Client;
+//using EventBus.Messages.Common;
+using HealthChecks.UI.Client;
 using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ordering.API.EventBusConsumer;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
-namespace Basket.API
+namespace Ordering.API
 {
     public class Startup
     {
@@ -33,38 +27,37 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Redis Configuration
-            services.AddStackExchangeRedisCache(options =>
-            {
-                options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
-            });
-
-            // General Configuration
-            services.AddScoped<IBasketRepository, BasketRepository>();
-            services.AddAutoMapper(typeof(Startup));
-
-            // Grpc Configuration
-            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
-                        (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
-            services.AddScoped<DiscountGrpcService>();
+            services.AddApplicationServices();
+            services.AddInfrastructureServices(Configuration);
 
             // MassTransit-RabbitMQ Configuration
             services.AddMassTransit(config => {
+
+                //config.AddConsumer<BasketCheckoutConsumer>();
+
                 config.UsingRabbitMq((ctx, cfg) => {
                     cfg.Host(Configuration["EventBusSettings:HostAddress"]);
                     cfg.UseHealthCheck(ctx);
+
+                    //cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c => {
+                    //    c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+                    //});
                 });
             });
             services.AddMassTransitHostedService();
 
+            // General Configuration
+            services.AddScoped<BasketCheckoutConsumer>();
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
             });
 
             services.AddHealthChecks()
-                    .AddRedis(Configuration["CacheSettings:ConnectionString"], "Redis Health", HealthStatus.Degraded);
+                    .AddDbContextCheck<OrderContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,7 +67,7 @@ namespace Basket.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API v1"));
             }
 
             app.UseRouting();
@@ -84,11 +77,11 @@ namespace Basket.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                //endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
-                //{
-                //    Predicate = _ => true,
-                //    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-                //});
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             });
         }
     }
